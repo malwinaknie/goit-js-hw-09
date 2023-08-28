e183c16dadad46d99a57a0b84cf322278d00db16
malwinaknie <109996482+malwinaknie@users.noreply.github.com>
2023-08-28 15:56:15 +0000

    Deploying to gh-pages from @ malwinaknie/goit-js-hw-09@64b763bbf2d98454fce0e3af469f27344bd1b686 🚀

---
+++ 01-color-switcher.21ac4639.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;t.addEventListener("click",(()=>{t.disabled=!0,e.disabled=!1,d=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,e.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.21ac4639.js.map
