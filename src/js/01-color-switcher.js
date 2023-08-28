64b763bbf2d98454fce0e3af469f27344bd1b686
Malwina Knie <malwinaknie@wp.pl>
2023-08-28 17:55:48 +0200

    Finished first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let timerId = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+
+  btnStart.addEventListener('click', () => {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+      }, 1000);
+  })
+
+  btnStop.addEventListener('click', () => {
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+    clearInterval(timerId);
+  })
